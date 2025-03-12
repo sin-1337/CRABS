6b2fc00d7ae98158098c5d5cfcacf32fde452032
Sin <bogusemail@nowhere.com>
2025-03-12 17:28:19 -0400

    updated package.json to fix missing comma, also added crabsloader.user.js

---
+++ crabsloader.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name CRABS
+// @namespace https://www.bondageprojects.com/
+// @version 0.0.1
+// @description Adds /players, shows info about players in the room, also adds /whisper+
+// @author Sin
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match https://www.bondageprojects.com/*
+// @match http://localhost:*/*
+// @icon data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+    var script = document.createElement("script");
+    script.langauge = "JavaScript";
+    script.setAttribute("crossorigin", "anonymous");
+    script.src = `https://sin-1337.github.io/CRABS/bundle.js?${Date.now()}`;
+    document.head.appendChild(script);
+})();
