588e3f7938152943984201706671758eba514046
Sin <bogusemail@nowhere.com>
2025-03-13 23:46:11 -0400

    testing dynamic reloads of CRABS take 3 with version increment

--- crabsloader.user.js
+++ crabsloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name CRABS
 // @namespace https://www.bondageprojects.com/
-// @version 0.0.2.11
+// @version 0.0.2.12
 // @description Adds /players, shows info about players in the room, also adds /whisper+
 // @author Sin
 // @match https://bondageprojects.elementfx.com/*
