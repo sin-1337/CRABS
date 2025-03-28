c9589915ebd537264e79a08bb468cfc1dbf765fe
Sin <bogusemail@nowhere.com>
2025-03-28 15:10:43 -0400

    Update version number to $VERSION

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import WhisperPlus from "./modules/whisperplus";
 
 
 // configure the version and mod name
-const VERSION = "1.0.0.44";
+const VERSION = "1.0.0.45";
 const NAME = "Crazy Roster Add-on By Sin";
 const NICKNAME = "CRABS";
 
