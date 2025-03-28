8205eb31cc20a6afa31482be41497072971182d8
Sin <bogusemail@nowhere.com>
2025-03-28 16:54:32 -0400

    Update version number to $VERSION

--- crabsloader.user.js
+++ crabsloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name CRABS
 // @namespace https://www.bondageprojects.com/
-// @version 1.0.0.51
+// @version 1.0.0.52
 // @description Adds /players, shows info about players in the room, also adds /whisper+
 // @author Sin
 // @match https://bondageprojects.elementfx.com/*
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import WhisperPlus from "./modules/whisperplus";
 
 
 // configure the version and mod name
-const VERSION = "1.0.0.51 Alpha";
+const VERSION = "1.0.0.52 Alpha";
 const NAME = "Crazy Roster Add-on By Sin";
 const NICKNAME = "CRABS";
 
