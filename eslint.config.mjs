8b79dc41a73ab31f2e29e0665414432dca4d9ddc
Sin <bogusemail@nowhere.com>
2025-04-11 14:13:21 -0400

    added globals.d.ts to clean up linter errors

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: 
+        { globals:
+            {
+                ...globals.browser,
+                ChatRoomSendLocal: "readonly",
+                CommandCombine: "readonly",
+                Commands: "writeable",
+                Player: "writeable",
+            }
+
+        } 
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+  { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
+  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+]);
---
+++ src/globals.d.ts
@@ -0,0 +1,112 @@
+// globals.d.ts
+declare global {
+  var ChatRoomCharacter: Array<any>;
+  var ChatRoomData: any;
+  var ChatRoomSendLocal: (message: string) => void;
+  var Commands: Array<any>;
+  var CurrentOnlinePlayers: number;
+  var data: {};
+
+  // unique to crabs
+  interface Window {
+      PlayerFocus: typeof Roster.showPlayerFocus;
+      sendWhisper: typeof WhisperPlus.sentWhisper;
+  }
+
+  interface Lovership {
+    Name: string;
+    MemberNumber?: number;
+    Stage?: 0 | 1 | 2;
+    Start?: number;
+  }
+
+  interface CharacterPoseMapping {
+    BodyLower?: string;
+    BodyFull?: string;
+    BodyAddon?: string;
+    [key: string]: any;
+  }
+
+  interface PlayerCharacter {
+    ID?: number;
+    Name: string;
+    MemberNumber?: number;
+    Type?: string;
+
+    ItemPermission: number;
+    LabelColor?: string;
+    LastChatRoom?: any;
+
+    Lover?: string;
+    Owner?: string;
+    Effect: string[];
+    Lovership: Lovership[];
+    Attribute: string[];
+    Appearance: any[];
+    Inventory: any[];
+    BlackList: Array<any>;
+    GhostList: Array<any>;
+    FriendList: Array<any>;
+    FriendNames: Map;
+    WhiteList: Array<any>;
+    BCT: any; // only for BCTweaks
+
+    PoseMapping: CharacterPoseMapping;
+    DrawPoseMapping: Record<string, any>;
+    ActivePoseMapping: Record<string, any>;
+    AllowedActivePoseMapping: Record<string, string[]>;
+
+    Stage: string;
+    CurrentDialog: string;
+
+    HasEffect(effect: string): boolean;
+    IsPlayer(): this is PlayerCharacter;
+    IsOwned(): boolean | string;
+    IsOwnedByCharacter(C: PlayerCharacter): boolean;
+    IsOwnedByPlayer(C: number): boolean;
+    IsLoverOfCharacter(C: PlayerCharacter): boolean;
+    GetLovership(MembersOnly?: boolean): Lovership[];
+    GetLoversNumbers(): Array<number>;
+    GetGenders(): string[];
+    GetPronouns(): string;
+    IsInFamilyOfMemberNumber(C: number): boolean;
+    OwnerNumber(): number;
+
+    ChatSettings: {
+        OOCAutoClose: boolean;
+    };
+
+
+    // You can add the rest of the methods as needed
+  }
+
+  var Player: PlayerCharacter;
+
+  function addChatMessage(msg: string): void;
+  function CommandCombine(command: Array<any>);
+  function CharacterNickname(Player: PlayerCharacter): string;
+  function ChatRoomFocusCharacter(player: PlayerCharacter): void;
+  function ChatRoomGenerateChatRoomChatMessage(
+      type: string, 
+      msg: string
+  ): {
+    Content: string,
+    Type: type,
+    Dictionary: {}
+  };
+  function ChatRoomRegisterMessageHandler(
+      message: {
+          Description: string,
+          Priority: number,
+          Callback: any,
+      }): any;
+  function ChatRoomSendLocalChatRoomSendLocal(text: string, value: numbeer): void;
+  function ChatRoomStatusUpdate(payload: string): any;
+  function ChatRoomMapViewCharacterOnWhisperRange(target: PlayerCharacter): boolean;
+  function ChatRoomMapViewIsActive(): boolean;
+  function ServerSend(Action: string, Payload: {Query: sring}): Promise;
+  function TextGet(text: string): void;
+  function TextGetInScope(path_to_csv: string, permission: string): void;
+}
+
+export {};
