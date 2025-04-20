b8674e25de0b729692cac6f7ef8743d397f05858
Sin <bogusemail@nowhere.com>
2025-04-20 05:11:53 -0400

    Update version number to 1.0.0.172

--- crabsloader.user.js
+++ crabsloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name CRABS
 // @namespace https://www.bondageprojects.com/
-// @version 1.0.0.171
+// @version 1.0.0.172
 // @description Adds /players, shows info about players in the room, also adds /whisper+
 // @author Sin
 // @match https://bondageprojects.elementfx.com/*
