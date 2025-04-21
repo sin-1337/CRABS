7de2ee8e3332151a6c6adbb5302eb217bec17c72
Sin <bogusemail@nowhere.com>
2025-04-21 02:42:24 -0400

    Update version number to 1.1.0.195

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import Banner from "./modules/banner";
 import Help from "./modules/help";
 
 // configure the version and mod name
-const VERSION = "1.1.0.194 Beta";
+const VERSION = "1.1.0.195 Beta";
 const NAME = "Crazy Roster Add-on By Sin";
 const NICKNAME = "CRABS";
 
