a7e979e8091cee273d68d2bce1e4d643aa5ea0e0
Sin <bogusemail@nowhere.com>
2025-06-04 01:30:07 -0400

    testing help button, with callback

--- src/base.ts
+++ src/base.ts
@@ -216,8 +216,8 @@ export default class CRABS {
     } else {
       console.log("CRABS ERROR: Could not find chat element!");
     }
-    this.attachEvent("CRABS_Help_Icon", "crabsHelp", undefined, VERSION);
-    //this.attachEventWithCallback("CRABS_Help_Icon", (e) => window.crabsHelp(e));
+    //this.attachEvent("CRABS_Help_Icon", "crabsHelp", undefined, VERSION);
+    this.attachEventWithCallback("CRABS_Help_Icon", (e) => window.crabsHelp(e));
     this.attachEvent("CRABS_close", "crabsCloseItem", "elementid");
   }
 
--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ window.sendWhisper = WHISPERPLUS.sendWhisper;
 window.PlayerFocus = ROSTER.showPlayerFocus;
 window.printRoster = ROSTER.printRoster;
 window.crabsCloseItem = ROSTER.closeElement;
-window.crabsHelp = Modules.Help.showHelp;
+window.crabsHelp = HELP.showHelp.bind(HELP);
 
 // print version and load success in console
 console.log(`CRABS v${VERSION} Loaded`);  // do not remove
@@ -86,7 +86,7 @@ ChatRoomRegisterMessageHandler({
 function argcheck(args: string): boolean {
   const SPLITARGS = args.split(" ");
   if (SPLITARGS[0].toLowerCase() == "help") {
-    HELP.sendoutput(Modules.Help.showHelp(VERSION), "CRABS_Help");
+    HELP.sendoutput(HELP.showHelp(), "CRABS_Help");
     const HELPBUTTON = document.getElementById("CRABS_Help_Icon")
     if (HELPBUTTON) HELPBUTTON.style.display = "none";
     return false;
