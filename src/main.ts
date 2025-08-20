b1a357d429a1ed8d937a78d20c5beb6f8ff0e245
Sin <bogusemail@nowhere.com>
2025-08-20 03:35:33 -0400

    Update version number to 1.3.1.70

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import bcModSDK from "bondage-club-mod-sdk";
 import * as Modules from "./modules";
 
 // configure the version and mod name
-(globalThis as any).VERSION = "1.3.1.69 Alpha";
+(globalThis as any).VERSION = "1.3.1.70 Alpha";
 (globalThis as any).NAME = "Crazy Roster Add-on By Sin";
 (globalThis as any).NICKNAME = "CRABS";
 
