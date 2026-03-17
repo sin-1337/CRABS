2e636f62524877747d06321234580e18d7be2a4e
Sin <bogusemail@nowhere.com>
2026-03-17 17:11:56 -0400

    pipeline work flow fixes

--- crabsloader.user.js
+++ crabsloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name CRABS
 // @namespace https://www.bondageprojects.com/
-// @version 1.3.2.14
+// @version 1.3.2.13
 // @description Adds /players, shows info about players in the room, also adds /whisper+
 // @author Sin
 // @match https://bondageprojects.elementfx.com/*
@@ -9,8 +9,9 @@
 // @match https://bondage-europe.com/*
 // @match https://www.bondage-europe.com/*
 // @match https://www.bondageprojects.com/*
+// @match https://www.bondage-asia.com/*
 // @match http://localhost:*/*
-// @icon https://sin-1337.github.io/CRABS/CRABS_Logo.png
+// @icon https://cdn.jsdelivr.net/gh/sin-1337/CRABS@__BRANCH__/images/CRABS_Logo.png
 // @grant none
 // @run-at document-end
 // ==/UserScript==
