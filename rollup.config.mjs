1f27311cf6e1010229c1c99edfd9cb8d15a1b76c
Sin <bogusemail@nowhere.com>
2026-03-17 15:36:46 -0400

    Update version number to 1.3.2.10

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -63,7 +63,7 @@ console.debug("CRABS: Parse start...");
 		preventAssignment: true,
 		NAME: JSON.stringify("Crazy Roster Add-on By Sin"),
 		NICKNAME: JSON.stringify("CRABS"),
-  		VERSION: JSON.stringify("1.3.2.9 Alpha"),
+  		VERSION: JSON.stringify("1.3.2.10 Alpha"),
 	}),
     progress({ clearLine: true }),
     resolve({ browser: true }),
