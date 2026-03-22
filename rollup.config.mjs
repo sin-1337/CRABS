1f9eb02efb111e08f27917a1f9fd4df19617b965
Sin <bogusemail@nowhere.com>
2026-03-22 14:15:29 -0400

    build #1.3.2.71

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -53,7 +53,7 @@ window.CRABS_Loaded = false;
 			preventAssignment: true,
 			NAME: JSON.stringify("Crazy Roster Add-on By Sin"),
 			NICKNAME: JSON.stringify("CRABS"),
-			VERSION: JSON.stringify("1.3.2.70 Alpha"),
+			VERSION: JSON.stringify("1.3.2.71 Alpha"),
 		}),
 		progress({ clearLine: true }),
 		resolve({ browser: true }),
