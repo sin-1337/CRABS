be563d4d4b3fe2d557ed68a791c8ab11890efc7c
Sin <bogusemail@nowhere.com>
2026-04-28 02:06:44 -0400

    resolve merge issues

--- crabsloader.user.js
+++ crabsloader.user.js
@@ -1,11 +1,7 @@
 // ==UserScript==
 // @name CRABS
 // @namespace https://www.bondageprojects.com/
-<<<<<<< HEAD
 // @version 2.1.0.97
-=======
-// @version 2.1.0.97
->>>>>>> Alpha
 // @description Adds /players, shows info about players in the room, also adds /whisper+
 // @author Sin
 // @match https://bondageprojects.elementfx.com/*
