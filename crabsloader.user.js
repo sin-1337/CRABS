7298f8d087833a089f19ea1a4cc48445a4e195f9
Sin <bogusemail@nowhere.com>
2026-04-29 10:26:55 -0400

    update logo in loader

--- crabsloader.user.js
+++ crabsloader.user.js
@@ -10,7 +10,7 @@
 // @match https://www.bondage-europe.com/*
 // @match https://www.bondageprojects.com/*
 // @match http://localhost:*/*
-// @icon https://sin-1337.github.io/CRABS/CRABS_Logo.png
+// @icon https://sin-1337.github.io/CRABS/images/CRABS_Logo.png
 // @grant none
 // @run-at document-end
 // ==/UserScript==
@@ -18,11 +18,11 @@
 
 
 
-(function() {
-    'use strict';
-    var script = document.createElement("script");
-    script.langauge = "JavaScript";
-    script.setAttribute("crossorigin", "anonymous");
-    script.src = `https://sin-1337.github.io/CRABS/Alpha/bundle.js?${Date.now()}`;
-    document.head.appendChild(script);
+(function () {
+	'use strict';
+	var script = document.createElement("script");
+	script.langauge = "JavaScript";
+	script.setAttribute("crossorigin", "anonymous");
+	script.src = `https://sin-1337.github.io/CRABS/Alpha/bundle.js?${Date.now()}`;
+	document.head.appendChild(script);
 })();
