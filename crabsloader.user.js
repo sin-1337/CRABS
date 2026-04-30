bea41a2fe1fb9da7d9a7831ea1eb4182af751b46
Sin <bogusemail@nowhere.com>
2026-04-29 23:15:29 -0400

    build #2.1.0.138

--- crabsloader.user.js
+++ crabsloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name CRABS
 // @namespace https://www.bondageprojects.com/
-// @version 2.1.0.137
+// @version 2.1.0.138
 // @description Adds /players, shows info about players in the room, also adds /whisper+
 // @author Sin
 // @match https://bondageprojects.elementfx.com/*
