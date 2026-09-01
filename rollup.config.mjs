8ea9431355ba7422b3eb73c4519daefa051a8c29
Sin <bogusemail@nowhere.com>
2026-09-01 09:26:51 -0400

    build #3.0.0.43

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -16,7 +16,7 @@ const require = createRequire(import.meta.url);
 const packageJson = require("./package.json");
 
 // 👇 THESE ARE THE LINES YOUR DEPLOY SCRIPT UPDATES! 👇
-const BUILD_VERSION = "3.0.0.42";
+const BUILD_VERSION = "3.0.0.43";
 const BUILD_BRANCH = "Alpha";
 
 export default {
