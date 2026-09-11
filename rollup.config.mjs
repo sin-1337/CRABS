1a2f5fb4fafedbc06bb2ed8417e8aad34e490c43
Sin <bogusemail@nowhere.com>
2026-09-11 16:04:43 -0400

    build #3.0.0.110

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -19,7 +19,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const require = createRequire(import.meta.url);
 const packageJson = require("./package.json");
 
-const BUILD_VERSION = "3.0.0.109";
+const BUILD_VERSION = "3.0.0.110";
 const BUILD_BRANCH = "Alpha";
 
 // Dynamic target resolution for test mode
